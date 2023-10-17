ebcab5667740126e6fdfe99da594e84c7164dbdb
talset <talset55@gmail.com>
2023-10-17 17:26:27 +0200

    terraform: update output to use vars

--- terraform-sample/terraform/module-sample/outputs.tf
+++ terraform-sample/terraform/module-sample/outputs.tf
@@ -1,15 +1,3 @@
 output "instance_id" {
   value = null_resource.instance.id
 }
-
-output "env" {
-  value = var.env
-}
-
-output "project" {
-  value = var.project
-}
-
-output "organization" {
-  value = var.organization
-}
